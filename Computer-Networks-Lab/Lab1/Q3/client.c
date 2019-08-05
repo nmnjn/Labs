7ce6ea692633cd361384bd10bed57fe824448851
Naman Jain <naman17@gmail.com>
2019-08-05 14:25:45 +0530

    added Lab1Q3

---
+++ Computer-Networks-Lab/Lab1/Q3/client.c
@@ -0,0 +1,32 @@
+#include "../../ServerHeader.h"
+
+void PerformClientTask(int sockfd){
+	int n = 1;
+	char ch[256], buf[256];
+
+	while(1){
+		n = 1;
+		printf("Enter String: ");
+		gets(ch);
+		ch[strlen(ch)] = '\0';
+		write(sockfd, ch, strlen(ch));
+		if (strcmp(ch, "quit") == 0)
+	    {
+	         printf("Quiting..");
+	         close(sockfd);
+	         exit(1);
+	    }
+		printf("Respone from the server is... ");
+		n = read(sockfd, buf, sizeof(buf));
+		puts(buf);
+	}
+}
+
+int main(){
+	int sockfd;
+	sockfd = CreateClientSocket();
+	PerformClientTask(sockfd);
+}
+
+
+
---
+++ Computer-Networks-Lab/Lab1/Q3/server.c
@@ -0,0 +1,39 @@
+#include "../../ServerHeader.h"
+#include <ctype.h>
+
+void PerformServerTask(int sockfd){
+	int newsockfd, clilen, n=1;
+	struct sockaddr_in seraddr, clientAddress;
+
+	while(1){
+		char buf[256];
+		memset(buf, 0, 255);
+		printf("\n%s", "server waiting for connection\n");
+		clilen = sizeof(clientAddress);
+		newsockfd = accept(sockfd, (struct sockaddr *)&clientAddress, &clilen);
+		printf("The Client IP is %u\n", clientAddress.sin_addr.s_addr);
+		printf("The Client Port is %u\n", clientAddress.sin_port);
+		n = read(newsockfd, buf, sizeof(buf));
+		if (strcmp(buf, "quit") == 0)
+	    {
+	         printf("\nQuiting Server\n");
+	         close(newsockfd);
+	         close(sockfd);
+	         exit(1);
+	    }
+
+		printf("\nMessage From Client: %s", buf);
+		for(int i=0;i<strlen(buf);i++)
+        	buf[i] = toupper(buf[i]);
+		n = write(newsockfd, buf, sizeof(buf));
+	}
+}
+
+int main(){
+	int sockfd;
+	sockfd = CreateServerSocket();
+	PerformServerTask(sockfd);
+}
+
+
+
