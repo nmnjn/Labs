f7701524f8e8250081775db0297305e974cd030d
Naman Jain <naman17@gmail.com>
2019-08-08 14:03:33 +0530

    Tower of Hanoi

--- Algorithms-Lab/Lab2/Tower of Hanoi/main.c
+++ Algorithms-Lab/Lab2/Tower of Hanoi/main.c
@@ -15,7 +15,7 @@ void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod)
   
 int main() 
 { 
-    int n = 4; // Number of disks 
+    int n = 3; // Number of disks 
     towerOfHanoi(n, 'A', 'C', 'B');  // A, B and C are names of rods 
     return 0; 
 }
