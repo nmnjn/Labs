4cf80cede341877ee5f5fed93e7089ccb43b8933
Naman Jain <naman17@gmail.com>
2019-08-30 14:30:39 +0530

    Diameter of a BST

---
+++ Algorithms-Lab/Lab5/Diameter/Diameter.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node {
+	int value;
+	struct node *left, *right;
+};
+
+typedef struct node * NODE;
+
+int max(int a, int b) {
+	return (a > b) ? a : b;
+}
+
+NODE newNode(int x) {
+	NODE root = (NODE)malloc(sizeof(struct node));
+	root->value = x;
+	root->left = root->right = NULL;
+	return root;
+}
+
+int calulateHeight(NODE root) {
+	if (root == NULL) {
+		return 0;
+	}
+	return 1 + max(calulateHeight(root->right), calulateHeight(root->left));
+}
+
+int calculateDiameter(NODE root) {
+
+	if (root == NULL) {
+		return 0;
+	}
+
+	int leftHeight = calulateHeight(root->left);
+	int rightHeight = calulateHeight(root->right);
+
+	int leftDiameter = calculateDiameter(root->left);
+	int rightDiameter = calculateDiameter(root->right);
+
+	return max((leftHeight + rightHeight + 1), max(leftDiameter, rightDiameter));
+
+}
+
+NODE insert(NODE node, int value) 
+{ 
+    if (node == NULL) return newNode(value); 
+    if (value < node->value) 
+        node->left  = insert(node->left, value); 
+    else if (value > node->value) 
+        node->right = insert(node->right, value);
+    return node; 
+} 
+
+void main() {
+
+	NODE root = NULL;
+	printf("Enter Values to insert into BST, enter -1 to stop\n");
+	int val = 0;
+	while(val != -1){
+		scanf("%d", &val);
+		if (val != -1)
+		{
+			root = insert(root, val);
+		}
+	}
+	printf("The diameter is: %d\n", calculateDiameter(root));
+}
